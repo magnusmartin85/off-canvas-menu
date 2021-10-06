9c7f609e8d7cdc2269ecb417dc847b4ef97622e9
magnusmartin85 <martin@magnusmartin.com>
2021-10-06 18:57:18 +0200

    REF: rename function.

--- resources/scripts/off-canvas-menu.js
+++ resources/scripts/off-canvas-menu.js
@@ -1,11 +1,11 @@
 import {CLASSNAME_ICON_CLOSE, CLASSNAME_ICON_OPEN, CLASSNAME_LINK_LEVEL_1, CLASSNAME_SIDEBAR} from './constants';
-import {addCloseEvents, addEventsLvl1, openMenu} from './functions';
+import {addCloseEvents, addEventsForLevel1Links, openMenu} from './functions';
 
 const body = document.querySelector('body');
 const iconOpen = document.querySelector('.' + CLASSNAME_ICON_OPEN);
 const linkLevel1 = document.querySelectorAll('.' + CLASSNAME_LINK_LEVEL_1);
 const sidebar = document.querySelector('.' + CLASSNAME_SIDEBAR);
-addEventsLvl1(linkLevel1);
+addEventsForLevel1Links(linkLevel1);
 
 iconOpen.addEventListener('click', () => {
   const iconClose = document.querySelector('.' + CLASSNAME_ICON_CLOSE);
