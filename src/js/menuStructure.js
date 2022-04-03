ed7232cf520a96f6238fee8e8e42bff47bbc11bf
magnusmartin85 <martin@magnusmartin.com>
2022-04-03 21:16:24 +0200

    [Feature] Define menu items in separate file

---
+++ src/js/menuStructure.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    level: 0,
+    title: "Home",
+    href: "#",
+    children: [],
+  },
+  {
+    level: 0,
+    title: "Services",
+    href: "#",
+    children: [
+      {
+        level: 1,
+        title: "UX-Design",
+        href: "#",
+        children:  [
+          {
+            level: 2,
+            title: "asasas",
+            href: "#",
+            children: []
+          }
+        ]
+      },
+      {
+        level: 1,
+        title: "Webdesign",
+        href: "#",
+        children:  []
+      },
+      {
+        level: 1,
+        title: "Content Marketing",
+        href: "#",
+        children:  []
+      }
+    ]
+  },
+  {
+    level: 0,
+    title: "Team",
+    children: [
+      {
+        level: 1,
+        title: "John",
+        href: "#",
+        children:  []
+      },
+      {
+        level: 1,
+        title: "Peter",
+        href: "#",
+        children:  []
+      },
+      {
+        level: 1,
+        title: "Michael",
+        href: "#",
+        children:  []
+      }
+    ]
+  },
+  {
+    level: 0,
+    title: "Contact",
+    href: "#",
+    children: []
+  },
+]
