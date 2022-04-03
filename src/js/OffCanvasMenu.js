d4853b4e0299f19996eac3b3f03393479ab29767
magnusmartin85 <martin@magnusmartin.com>
2022-04-03 21:32:50 +0200

    [Feature] Update functions

--- src/js/OffCanvasMenu.js
+++ src/js/OffCanvasMenu.js
@@ -3,25 +3,30 @@ import {
   CLASSNAME_BACKDROP,
   CLASSNAME_ICON_CLOSE,
   CLASSNAME_ICON_OPEN,
-  CLASSNAME_LINK_LEVEL_1,
+  CLASSNAME_LINK_LEVEL,
   CLASSNAME_SIDEBAR,
-  CLASSNAME_SUBMENU_LEVEL_1
-} from './constants';
-
+  CLASSNAME_SUBMENU_LEVEL,
+  MENU_TITLE,
+} from "./constants";
+import MarkupGenerator from "./MarkupGenerator/MarkupGenerator";
 
 class OffCanvasMenu {
   /**
+   * @param {string} menuId
    * @param {boolean} closeOtherSubmenus
    * @param {boolean} closeMenuOnBackdropClick
    * @param {boolean} closeSubmenusOnMenuClose
    */
   constructor(
+    menuId,
     closeOtherSubmenus,
     closeMenuOnBackdropClick,
     closeSubmenusOnMenuClose
   ) {
+    this.menuId = menuId;
+
     /**
-     * Level 1 link lists with css class 'link-level-1' are closed when another level 1 lists is clicked.
+     * Level 1 link lists with css class 'link-level-1' are closed when another level 1 list is clicked.
      * Only one level 1 link list can be open at once.
      * @type {boolean}
      */
@@ -40,16 +45,32 @@ class OffCanvasMenu {
     this.closeSubmenusOnMenuClose = closeSubmenusOnMenuClose;
   }
 
-  body = document.querySelector('body');
-  iconOpen = document.querySelector('.' + CLASSNAME_ICON_OPEN);
-  linkLevel1 = document.querySelectorAll('.' + CLASSNAME_LINK_LEVEL_1);
-  sidebar = document.querySelector('.' + CLASSNAME_SIDEBAR);
+  body = document.querySelector("body");
+  iconOpen = document.querySelector("." + CLASSNAME_ICON_OPEN);
+  sidebar = document.querySelector("." + CLASSNAME_SIDEBAR);
 
   init() {
-    this.addEventsForLinksLevel1(this.linkLevel1);
+    const mg = new MarkupGenerator("off-canvas-body");
 
-    this.iconOpen.addEventListener('click', () => {
-      const iconClose = document.querySelector('.' + CLASSNAME_ICON_CLOSE);
+    mg.init();
+
+    this.waitForDomElement(".off-canvas-nav").then(() => {
+      const linkLevel1 = document.querySelectorAll(
+        "." + CLASSNAME_LINK_LEVEL[1]
+      );
+
+      const linkLevel2 = document.querySelectorAll(
+        "." + CLASSNAME_LINK_LEVEL[2]
+      );
+
+      this.addEventsForLinks(linkLevel1, 1);
+      this.addEventsForLinks(linkLevel2, 2);
+
+      this.setMenuTitle(MENU_TITLE);
+    });
+
+    this.iconOpen.addEventListener("click", () => {
+      const iconClose = document.querySelector("." + CLASSNAME_ICON_CLOSE);
       let closingItems = [iconClose];
 
       this.openMenu(this.sidebar, this.body);
@@ -58,8 +79,8 @@ class OffCanvasMenu {
   }
 
   addBackdropToDom() {
-    const backdropDiv = document.createElement('div');
-    backdropDiv.className = 'off-canvas-backdrop';
+    const backdropDiv = document.createElement("div");
+    backdropDiv.className = "off-canvas-backdrop";
     document.body.appendChild(backdropDiv);
   }
 
@@ -71,12 +92,12 @@ class OffCanvasMenu {
    */
   addCloseEvents(closingItems, sidebar, body) {
     if (this.closeMenuOnBackdropClick) {
-      const backdrop = document.querySelector('.' + CLASSNAME_BACKDROP);
+      const backdrop = document.querySelector("." + CLASSNAME_BACKDROP);
       closingItems.push(backdrop);
     }
 
     for (let i = 0; i < closingItems.length; i++) {
-      closingItems[i].addEventListener('click', () => {
+      closingItems[i].addEventListener("click", () => {
         this.hideSidebar(sidebar);
         this.removeBackdropFromDom();
         this.makeBodyScrollable(body);
@@ -88,40 +109,56 @@ class OffCanvasMenu {
 
   /**
    * Attach click listener to level 1 menu item links.
-   * @param {NodeList} linksLevel1 - List of level 1 links.
+   * @param {NodeList} links - List of links.
+   * @param {number} level - Number of level to add events for.
    */
-  addEventsForLinksLevel1(linksLevel1) {
-    for (let i = 0; i < linksLevel1.length; i++) {
-      if (linksLevel1[i].children.length > 0) {
-        linksLevel1[i].addEventListener('click', evt => {
-          this.handleClickOnLevel1(evt);
-        });
-      }
-    }
+  addEventsForLinks(links, level) {
+    links &&
+      links.forEach((link) => {
+        link &&
+          link.addEventListener("click", (evt) => {
+            this.handleClickOnLevel(evt, level);
+          });
+      });
   }
 
   closeAllSubmenus() {
-    const submenuItems = document.querySelectorAll('.' + CLASSNAME_SUBMENU_LEVEL_1);
+    const submenuItems = document.querySelectorAll(
+      "." + CLASSNAME_SUBMENU_LEVEL[1],
+      "." + CLASSNAME_SUBMENU_LEVEL[2]
+    );
+    console.warn(submenuItems);
     for (let i = 0; i < submenuItems.length; i++) {
-      submenuItems[i].style.display = 'none';
+      submenuItems[i].style.display = "none";
     }
   }
 
   /**
    *
    * @param {object} evt
+   * @param {number} level
    */
-  handleClickOnLevel1(evt) {
-    let isOpenLevel1 = evt.currentTarget.closest('.' + CLASSNAME_LINK_LEVEL_1).nextElementSibling.style.display === 'block';
+  handleClickOnLevel(evt, level) {
+    let isOpen =
+      evt.currentTarget.closest("." + CLASSNAME_LINK_LEVEL[level] + "")
+        .parentElement.nextElementSibling.style.display === "block";
 
-    if (isOpenLevel1) {
-      evt.currentTarget.closest('.' + CLASSNAME_LINK_LEVEL_1).parentNode.childNodes[3].style.display = 'none';
-      evt.currentTarget.closest('.' + CLASSNAME_LINK_LEVEL_1).childNodes[1].classList.remove('rotate-90');
+    if (isOpen) {
+      evt.currentTarget.closest(
+        "." + CLASSNAME_LINK_LEVEL[level]
+      ).parentElement.nextElementSibling.style.display = "none";
+      evt.currentTarget
+        .closest("." + CLASSNAME_LINK_LEVEL[level])
+        .childNodes[1].classList.remove("rotate-90");
     } else {
       this.closeOtherSubmenus && this.closeAllSubmenus();
       this.closeOtherSubmenus && this.rotateMenuArrowsBack();
-      evt.currentTarget.closest('.' + CLASSNAME_LINK_LEVEL_1).parentNode.childNodes[3].style.display = 'block';
-      evt.currentTarget.closest('.' + CLASSNAME_LINK_LEVEL_1).childNodes[1].classList.add('rotate-90');
+      evt.currentTarget.closest(
+        "." + CLASSNAME_LINK_LEVEL[level]
+      ).parentElement.nextElementSibling.style.display = "block";
+      evt.currentTarget
+        .closest("." + CLASSNAME_LINK_LEVEL[level])
+        .childNodes[1].classList.add("rotate-90");
     }
   }
 
@@ -130,8 +167,8 @@ class OffCanvasMenu {
    * @param {object} sidebar - DOM Element which contains the menu.
    */
   handleSidebar(sidebar) {
-    sidebar.className = CLASSNAME_SIDEBAR + ' show';
-    sidebar.style.visibility = 'visible';
+    sidebar.className = CLASSNAME_SIDEBAR + " show";
+    sidebar.style.visibility = "visible";
   }
 
   /**
@@ -147,7 +184,7 @@ class OffCanvasMenu {
    * @param {object} body - DOM Element which contains the body.
    */
   makeBodyScrollable(body) {
-    body.style.overflow = '';
+    body.style.overflow = "";
   }
 
   /**
@@ -159,7 +196,6 @@ class OffCanvasMenu {
     this.preventBodyFromScrolling(body);
     this.handleSidebar(sidebar);
     this.addBackdropToDom();
-    this.setPageYOffset();
   }
 
   /**
@@ -167,16 +203,16 @@ class OffCanvasMenu {
    * @param {object} body - DOM Element which contains the body.
    */
   preventBodyFromScrolling(body) {
-    body.style.overflow = 'hidden';
+    body.style.overflow = "hidden";
   }
 
   removeBackdropFromDom() {
-    const backdropDiv = document.querySelector('.off-canvas-backdrop');
+    const backdropDiv = document.querySelector(".off-canvas-backdrop");
     backdropDiv && backdropDiv.remove();
   }
 
   rotateMenuArrowsBack() {
-    let menuArrows = document.querySelectorAll('.' + CLASSNAME_ARROW);
+    let menuArrows = document.querySelectorAll("." + CLASSNAME_ARROW);
     let len = menuArrows.length;
 
     for (let i = 0; i < len; i++) {
@@ -184,12 +220,32 @@ class OffCanvasMenu {
     }
   }
 
-  setPageYOffset() {
-    if (CLASSNAME_SIDEBAR.length === 0) {
-      window.requestAnimationFrame(this.setPageYOffset);
-    } else {
-      document.getElementsByClassName(CLASSNAME_SIDEBAR)[0].style.top = window.pageYOffset.toString() + 'px';
-    }
+  /**
+   * @param {string} title
+   */
+  setMenuTitle(title) {
+    const headline = document.querySelector(".off-canvas-title");
+    headline.innerHTML = title;
+  }
+
+  waitForDomElement(selector) {
+    return new Promise((resolve) => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(() => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+      });
+    });
   }
 }
 
