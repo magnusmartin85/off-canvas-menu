a163ecda2eb568b357769c698a6cb3f67a79cf7c
magnusmartin85 <martin@magnusmartin.com>
2022-04-24 21:10:10 +0200

    [Feature]: add webpack configs.

---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,41 @@
+const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Webpack = require("webpack");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  stats: "errors-only",
+  bail: true,
+  output: {
+    filename: "js/off-canvas-menu.min.js"
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[chunkhash:8].css",
+      chunkFilename: "css/[name].[chunkhash:8].chunk.js"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.s?css/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+});
