ed983003619579d108d98afeff544cfe52c46f83
magnusmartin85 <martin@magnusmartin.com>
2022-09-09 19:40:05 +0200

    [Feature] add js minify for webpack production mode- pt2

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,11 @@
-const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
+const { merge } = require("webpack-merge");
 
 module.exports = merge(common, {
   mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 });
