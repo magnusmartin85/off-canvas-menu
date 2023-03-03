5ea8c0cd8b9f1337e846de0ac4d72af38a211cb2
magnusmartin85 <martin@magnusmartin.com>
2023-03-03 22:55:55 +0100

    [Chore] remove old folders

--- src/js/index.ts
+++
@@ -1,8 +0,0 @@
-import "../scss/off-canvas-menu.scss";
-import OffCanvasMenu from "./OffCanvasMenu";
-import { MENU_ID } from "./constants";
-
-// TODO: Close other submenus not working with menu item nesting > 2
-const menu = new OffCanvasMenu(MENU_ID, false, true, true);
-
-menu.init();
